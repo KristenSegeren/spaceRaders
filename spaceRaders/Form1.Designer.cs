3297bacd61786d4c9750a2a5077ae2d9046c8e4c
KristenSegeren <krissege449@ed.amdsb.ca>
2023-05-18 23:16:28 -0400

    Add project files.

---
+++ spaceRaders/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace spaceRaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p1scoreLabel = new System.Windows.Forms.Label();
+            this.p2scoreLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.tlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p1scoreLabel
+            // 
+            this.p1scoreLabel.AutoSize = true;
+            this.p1scoreLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1scoreLabel.Location = new System.Drawing.Point(21, 404);
+            this.p1scoreLabel.Name = "p1scoreLabel";
+            this.p1scoreLabel.Size = new System.Drawing.Size(34, 37);
+            this.p1scoreLabel.TabIndex = 0;
+            this.p1scoreLabel.Text = "0";
+            // 
+            // p2scoreLabel
+            // 
+            this.p2scoreLabel.AutoSize = true;
+            this.p2scoreLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2scoreLabel.Location = new System.Drawing.Point(754, 404);
+            this.p2scoreLabel.Name = "p2scoreLabel";
+            this.p2scoreLabel.Size = new System.Drawing.Size(34, 37);
+            this.p2scoreLabel.TabIndex = 1;
+            this.p2scoreLabel.Text = "0";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Black;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-13, -19);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(820, 496);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Click the space bar to start";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tlabel
+            // 
+            this.tlabel.AutoSize = true;
+            this.tlabel.Font = new System.Drawing.Font("Maiandra GD", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tlabel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.tlabel.Location = new System.Drawing.Point(144, 156);
+            this.tlabel.Name = "tlabel";
+            this.tlabel.Size = new System.Drawing.Size(533, 56);
+            this.tlabel.TabIndex = 3;
+            this.tlabel.Text = "Welcome to Space Race";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tlabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.p2scoreLabel);
+            this.Controls.Add(this.p1scoreLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p1scoreLabel;
+        private System.Windows.Forms.Label p2scoreLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label tlabel;
+    }
+}
+
---
+++ spaceRaders/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace spaceRaders
+{
+ 
+
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(100, 325, 20, 20);
+        Rectangle player2 = new Rectangle(400, 325, 20, 20);
+
+        int ballSpeed = 8;
+        int ballSize = 10;
+       
+
+        int player1Score = 0;
+        int player2Score = 0;
+        int playerSpeed = 5;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool spacedown = false;
+       bool timer = false;
+
+        string state = "title";
+        List<Rectangle> leftballList = new List<Rectangle>();
+        List<Rectangle> rightballList = new List<Rectangle>();
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush clearBrush = new SolidBrush(Color.Black);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        #region keycode
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Space:
+                   state = "playing";
+                    titleLabel.Visible=false;
+                    tlabel.Visible=false;
+                    break;
+
+            }
+           
+        }
+
+
+        public void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+
+            }
+        }
+        #endregion
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (state == "playing")
+            {
+                #region leftside
+
+                //generate new left asteroid if it is time
+                randValue = randGen.Next(0, 100);
+
+                if (randValue <= 10)
+                {
+                    randValue = randGen.Next(10, this.Height - ballSize - 10);
+                    Rectangle ball = new Rectangle(0, randValue - 80, ballSize, ballSize);
+                    leftballList.Add(ball);
+                }
+
+                // move left asteroid
+                for (int i = 0; i < leftballList.Count; i++)
+                {
+                    int x = leftballList[i].X + ballSpeed;
+                    leftballList[i] = new Rectangle(x, leftballList[i].Y, ballSize, ballSize);
+                }
+
+                //remove left asteroid when it gets to the right side
+                for (int i = 0; i < leftballList.Count; i++)
+                {
+                    if (leftballList[i].X >= 818)
+                    {
+                        leftballList.RemoveAt(i);
+                    }
+                }
+
+                #endregion
+
+                #region rightside
+                //generate new left asteroid if it is time
+                randValue = randGen.Next(0, 100);
+
+                if (randValue <= 10)
+                {
+                    randValue = randGen.Next(10, this.Height - ballSize - 10);
+                    Rectangle ball = new Rectangle(800, randValue - 80, ballSize, ballSize);
+                    rightballList.Add(ball);
+                }
+
+                // move left asteroid
+                for (int i = 0; i < rightballList.Count; i++)
+                {
+                    int x = rightballList[i].X - ballSpeed;
+                    rightballList[i] = new Rectangle(x, rightballList[i].Y, ballSize, ballSize);
+                }
+
+                //remove left asteroid when it gets to the right side
+                for (int i = 0; i < rightballList.Count; i++)
+                {
+                    if (rightballList[i].X <= 0)
+                    {
+                        rightballList.RemoveAt(i);
+                    }
+                }
+                #endregion
+
+                #region moveplayer
+                //move player
+                if (wDown == true && player1.Y > 0)
+                {
+                    player1.Y -= playerSpeed;
+                }
+
+                if (sDown == true && player1.Y < this.Height - 40 - player1.Height)
+                {
+                    player1.Y += playerSpeed;
+                }
+
+                //move player 2
+                if (upArrowDown == true && player2.Y > 0)
+                {
+                    player2.Y -= playerSpeed;
+                }
+
+                if (downArrowDown == true && player2.Y < this.Height - 40 - player2.Height)
+                {
+                    player2.Y += playerSpeed;
+                }
+                #endregion
+
+                #region ballcontact
+                for (int i = 0; i < rightballList.Count; i++)
+                {
+                    if (player1.IntersectsWith(rightballList[i]))
+                    {
+                        player1.X = 100;
+                        player1.Y = 325;
+                    }
+                }
+                for (int i = 0; i < leftballList.Count; i++)
+                {
+                    if (player1.IntersectsWith(leftballList[i]))
+                    {
+                        player1.X = 100;
+                        player1.Y = 325;
+                    }
+                }
+                for (int i = 0; i < rightballList.Count; i++)
+                {
+                    if (player2.IntersectsWith(rightballList[i]))
+                    {
+                        player2.X = 400;
+                        player2.Y = 325;
+                    }
+                }
+                for (int i = 0; i < leftballList.Count; i++)
+                {
+                    if (player2.IntersectsWith(leftballList[i]))
+                    {
+                        player2.X = 400;
+                        player2.Y = 325;
+                    }
+                }
+
+                #endregion
+
+                #region pointcode
+                if (player1.Y == 0)
+                {
+                    player1Score++;
+                    player1.X = 100;
+                    player1.Y = 325;
+                }
+                if (player2.Y == 0)
+                {
+                    player2Score++;
+                    player2.X = 400;
+                    player2.Y = 325;
+
+                }
+                p1scoreLabel.Text = $"{player1Score}";
+                p2scoreLabel.Text = $"{player2Score}";
+
+                if (player1Score == 3)
+                {
+
+                    state = "winner";
+
+                }
+                if (player2Score == 3)
+                {
+                   
+                    state = "winner";
+                }
+                if (state == "winner" && player1Score == 3)
+                {
+                    titleLabel.Visible= true;
+                    titleLabel.Text = $"Player 1 is the Winner!!";
+                }
+                if (state == "winner" && player2Score == 3)
+                {
+                    titleLabel.Visible= true;
+                    titleLabel.Text = $"Player 2 is the Winner!!";
+                }
+                #endregion
+
+            }
+            Refresh();
+        }
+        #region paint
+       // if (timer = true)
+            
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (state == "playing")
+
+                
+                //draw hero
+                e.Graphics.FillRectangle(whiteBrush, player1);
+                e.Graphics.FillRectangle(whiteBrush, player2);
+
+                //draw balls
+                for (int i = 0; i < leftballList.Count; i++)
+                {
+                    e.Graphics.FillEllipse(whiteBrush, leftballList[i]);
+                }
+                for (int i = 0; i < rightballList.Count; i++)
+                {
+                    e.Graphics.FillEllipse(whiteBrush, rightballList[i]);
+                }
+        }
+        #endregion
+
+        
+    }
+
+
+    }
+
+
+
